27e5cf1085f1560d809f590503c80081e4d0405a
Wasay Qureshi <wasayq5@outlook.com>
2023-09-17 23:41:07 -0400

    added divisions for each note for distinction

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -1,15 +1,18 @@
+import React from "react";
 import notesStore from "../stores/notesStore";
 import Note from "./Note";
+import '../styles.css'; // Import the CSS
 
 export default function Notes() {
-    const store = notesStore();
+  const store = notesStore();
 
-    return (
-        <div>
-            <h2>Notes:</h2>
-            {store.notes && store.notes.map(note => {
-            return <Note note={note} key={note._id} />;
-            })}
+  return (
+    <div>
+      {store.notes && store.notes.map((note) => (
+        <div className="note" key={note._id}>
+          <Note note={note} />
         </div>
-    )
+      ))}
+    </div>
+  );
 }
