bf5691f5822f05c989f7f90a4d31b610779827a7
Wasay Qureshi <wasayq5@outlook.com>
2023-09-17 22:06:35 -0400

    added expand button on notes page to see note body, added styling to the whole app

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,20 +1,3 @@
-/* import notesStore from "../stores/notesStore"
-
-export default function Note({ note }) {
-    const store = notesStore(store => {
-        return { deleteNote: store.deleteNote, toggleUpdate: store.toggleUpdate };
-    });
-
-    return (
-    <div key={note._id}>
-    <h3>{note.title}</h3>
-    <h4>{note.body}</h4>
-    <button onClick={() => store.deleteNote(note._id)}>Delete Note</button>
-    <button onClick={() => store.toggleUpdate(note)}>Update Note</button>
-    </div>
-    )
-} */
-
 import React, { useState } from "react";
 import notesStore from "../stores/notesStore";
 
